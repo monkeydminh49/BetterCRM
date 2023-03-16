297e9c2f24780ffff95ae9f82124e26b4273e9fd
MinhDunk <nguyendangminh49@gmail.com>
2023-03-16 21:49:27 +0700

    New request obtain class id

--- src/main/java/Controller/Request.java
+++ src/main/java/Controller/Request.java
@@ -27,13 +27,9 @@ import java.io.File;
 public class Request {
 
     public static void main(String[] args) throws IOException {
-        List<NameValuePair> payload = new ArrayList<>();
-        payload.add(new BasicNameValuePair("username", "dangminh.TAMD"));
-        payload.add(new BasicNameValuePair("password", "LLVN123456"));
-        payload.add(new BasicNameValuePair("remember", "true"));
-
-
-        HttpGet get = new HttpGet("https://crm.llv.edu.vn/index.php?module=Users&action=Login");
+        // First Get request to get token
+        String loginUrl = "https://crm.llv.edu.vn/index.php?module=Users&action=Login&mode=login";
+        HttpGet get = new HttpGet(loginUrl);
         HttpClient client = HttpClients.createDefault();
         HttpResponse getResponse = client.execute(get);
 
@@ -49,48 +45,59 @@ public class Request {
         //System.out.println("Content:");
         //System.out.println(content);
 
-
-        String El1Url = "https://crm.llv.edu.vn/index.php?module=Classes&relatedModule=SJLessonContent&view=Detail&record=431598&mode=showRelatedList&tab_label=Lesson%20Content";
-        Document doc = (Document) Jsoup.connect(El1Url).get();
+        // Get token in returned HTML
+        Document doc = (Document) Jsoup.connect(loginUrl).get();
         //doc.select("input").forEach(System.out::println);
 
         Element element = doc.select("input").get(3);
         String token = element.attr("value");
         System.out.println("Token: " + token);
 
-        // Add token to payload
+        // Add form data to payload
+        List<NameValuePair> payload = new ArrayList<>();
+        payload.add(new BasicNameValuePair("username", "dangminh.TAMD"));
+        payload.add(new BasicNameValuePair("password", "LLVN123456"));
+        payload.add(new BasicNameValuePair("remember", "true"));
         payload.add(new BasicNameValuePair("__vtrftk", token));
 
         // Add payload to post request
-        String LoginUrl = "https://crm.llv.edu.vn/index.php?module=Users&action=Login&mode=login";
-        HttpPost httppost = new HttpPost(LoginUrl);
+        HttpPost httppost = new HttpPost(loginUrl);
         UrlEncodedFormEntity entity = new UrlEncodedFormEntity(payload, Consts.UTF_8);
         httppost.setEntity(entity);
         httppost.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36");
         CloseableHttpResponse response = (CloseableHttpResponse) client.execute(httppost);
 
-        HttpGet get2 = new HttpGet(El1Url);
+        // Make Get request to obtain data of Lesson content page of class EL1-2201
+        String LM1Url = "https://crm.llv.edu.vn/index.php?module=Classes&relatedModule=SJLessonContent&view=Detail&record=453551&mode=showRelatedList&tab_label=Lesson%20Content";
+        String on_goingClassListUrl = "https://crm.llv.edu.vn/index.php?module=Classes&parent=&page=1&view=List&viewname=493&orderby=schools&sortorder=ASC&search_params=%5B%5B%5B%22schools%22%2C%22c%22%2C%22MD%22%5D%5D%5D";
+        HttpGet get2 = new HttpGet(on_goingClassListUrl);
         response = (CloseableHttpResponse) client.execute(get2);
         content = IOUtils.toString(response.getEntity().getContent(), "UTF-8");
         //System.out.println(content);
         doc = (Document) Jsoup.parse(content);
 
-        element  = doc.select(".span2").get(6);
-        System.out.println(element.text());
-//        doc.select(".span2").forEach(System.out::println);
-
-        Elements elements = doc.select(".item__lesson");
+        // Get class id
+        Elements elements = doc.select("tr");
         for (Element e : elements) {
-            System.out.println(e.select("#lessonName").text() + " - " + e.select(".lessonEmailStatus").text());
+            if (e.hasClass("listViewEntries")){
+                System.out.println(e.attr("data-id"));
+                // System.out.println(e.select(".listViewEntryValue").get(0).text());
+            }
+            // System.out.println(e);
         }
 
 
+        // Get class name
+        //element  = doc.select(".span2").get(6);
+        //System.out.println(element.text());
 
+        // Get lessonName and lessonEmailStatus
+        //elements = doc.select(".item__lesson");
+        //for (Element e : elements) {
+        //    System.out.println(e.select("#lessonName").text() + " - " + e.select(".lessonEmailStatus").text());
+        //}
 
-//        String html = Jsoup.connect(El1Url).get().html();
-//        System.out.println(html);
-//
-//        File file = new File("El1.html");
+//        File file = new File("on-goingClassList.html");
 //        FileUtils.writeStringToFile(file, content, "UTF-8");
 
     }
