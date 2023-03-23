65c81b653b788279ddd0a8ea8446f79cf0622d6b
Nguyen Dang Minh <nguyendangminh49@gmail.com>
2023-03-24 01:00:13 +0700

    second commit test

--- src/main/java/com/mycompany/bettercrmv2/BetterCRMV2.java
+++ src/main/java/com/mycompany/bettercrmv2/BetterCRMV2.java
@@ -1,9 +1,11 @@
+package com.mycompany.bettercrmv2;
+
 import Viewer.GUIV2;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-public class Main {
+public class BetterCRMV2 {
     public static void main(String[] args) throws IOException, URISyntaxException, ClassNotFoundException {
 //        Request.getInstance().run();
         new GUIV2().setVisible(true);
